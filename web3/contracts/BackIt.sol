b6f68a1057a8a179c642251b4492f05bdae8a4b2
Rohan Nayan <rohannayan405@gmail.com>
2024-03-12 14:24:40 +0530

    Smart Contract Structure Created

--- web3/contracts/BackIt.sol
+++ web3/contracts/BackIt.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.9;
 
 contract BackIt {
-    constructor() {}
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donaters;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;  //TO MAP THROUGH Campaign structure => Campaign[0]
+
+    uint256 public numberOfCampaigns = 0;   //keeping track of no. of campaign inorder to give them Ids
 }
