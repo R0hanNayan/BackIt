df720bcceab474168e2665ea4eaf3324e1075229
Rohan Nayan <rohannayan405@gmail.com>
2024-03-12 14:37:41 +0530

    Added Create Campaign function

--- web3/contracts/BackIt.sol
+++ web3/contracts/BackIt.sol
@@ -17,4 +17,41 @@ contract BackIt {
     mapping(uint256 => Campaign) public campaigns;  //TO MAP THROUGH Campaign structure => Campaign[0]
 
     uint256 public numberOfCampaigns = 0;   //keeping track of no. of campaign inorder to give them Ids
+
+    function createCampaign(address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image) public returns (uint256){
+        //memory keyword required with every string
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        //to ckeck if everything is okay?
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1; //index of the most newly created campaign
+    }
+
+    function donateToCampaign() {
+        
+    }
+
+    function getDonators() {
+        
+    }
+
+    function getCampaigns() {
+        
+    }
 }
