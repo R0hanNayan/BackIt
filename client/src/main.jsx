b10131c563ba9862784bf13bba198283b2f45c68
Rohan Nayan <rohannayan405@gmail.com>
2024-03-14 13:58:33 +0530

    Configured Tailwind CSS

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import App from './App';
 
 
 const root = ReactDom.createRoot(document.getElementById('root'));
