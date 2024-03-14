35f6841097f2c10f602d87c1cfc56566c51a7ca0
Rohan Nayan <rohannayan405@gmail.com>
2024-03-15 01:29:01 +0530

    Initialised Pages and App.jsx Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative sm:-8 p-4 bg-[#13131A] min-h-screen flex flex-row'>
+      <div className='sm:flex hidden mr-10'>
+        Sidebar
+      </div>
+
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+        Navbar
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </div> 
+    </div>
   )
 }
 
