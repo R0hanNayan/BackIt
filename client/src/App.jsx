d101006b3e907080ab930a6d6b4dbeb3053868c3
Rohan Nayan <rohannayan405@gmail.com>
2024-03-15 01:32:34 +0530

    Initialised Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Sidebar, Navbar } from './components';
 import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
 const App = () => {
   return (
     <div className='relative sm:-8 p-4 bg-[#13131A] min-h-screen flex flex-row'>
       <div className='sm:flex hidden mr-10'>
-        Sidebar
+        <Sidebar />
       </div>
 
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-        Navbar
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />}/>
         </Routes>
