1abce01ed6f04eabfb4ded89a90808d2c32a237e
Rohan Nayan <rohannayan405@gmail.com>
2024-03-20 00:19:25 +0530

    Initialised Create Campaign

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';  //utility library to interact with utility library
+import { money } from '../assets';
+import { CustomButton } from '../components';
+import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  //Details required to create a campaign
+  const [form, setForm] = useState({
+    name: '',
+    title:'',
+    description:'',
+    target:'',
+    deadline:'',
+    image:''
+  });
+
   return (
-    <div>CreateCampaign</div>
+    <div className='bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4'>
+      Create
+    </div>
   )
 }
 
